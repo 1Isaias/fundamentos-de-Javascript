7510c3b99e50c94cc0cda28dc7b39f6cd3ec837c
Isaías Fonseca <1isaiasfonseca@gmail.com>
2022-10-27 11:10:59 -0300

    callbacks y promesas

---
+++ 03-wewbpack-inicial/src/index.js
@@ -0,0 +1,24 @@
+
+import{saludar} from './js/componentes';
+import './styles.css';
+
+const nombre = 'Isaias';
+
+saludar(nombre);
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+El webpack es una herramienta muy popular utilizada por casi todos los frameworks, la idea es que usemos webpack para generar un monton de tareas automaticas por nosotros 
+ Eso va desde: copiar y mover archivos, cambiar nombres, hacer inyecciones en el HTML de manera automatica, minimizar codigo, etc.
+ */
